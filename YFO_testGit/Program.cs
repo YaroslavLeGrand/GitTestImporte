aaa107eb4c87fc30687d4505a45c42c484a9e29a
YaroslavLeGrand <yaroslav.follet@campus-la-chataigneraie.org>
2023-05-17 13:27:07 +0200

    premier commit / 0.001A

---
+++ YFO_testGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YFO_testGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello my World");
+        }
+    }
+}
